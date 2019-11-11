3c20a12544f4b75f9a012049f464c71e53fbcc45
Jiang-Xuan <645762213@qq.com>
2019-11-11 13:08:52 +0800

    将 image 复制进 clipboard

--- Program.cs
+++ Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Image image = Resource1.visual_studio_logo;
+            Clipboard.SetImage(image);
         }
     }
 }
